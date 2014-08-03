5798145d41afee50b143b328cfdf387b29cbb5c6
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-08-03 18:19:54 -0400

    Added the files for friendships and some code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  
   validates :email, :first_name, :last_name, :password_confirmation, :password, presence: true
   validates :email, uniqueness: true
 
