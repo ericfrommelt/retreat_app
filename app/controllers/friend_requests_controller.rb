88d21cce056198c38ebac8ad541e8db66d0c9bc0
truotaku <najee.s.gardner@gmail.com>
2014-08-06 09:33:22 -0400

    tweaks to the friendship system

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -7,24 +7,16 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.create(friend_request_params)
   end
 
-  def destroy
-    @friend_request = FriendRequest.find(params[:id])
-    @friend_request.destroy
-  end
-
   def accept
-    @friend_request = FriendRequest.find(params[:id])
-    @friendship = Friendship.create(user: @friend_request.user, friend: @friend_request.sender)
+    @friend_request = FriendRequest.find(params[:friend_request_id])
+    Friendship.create(user: @friend_request.user, friend: @friend_request.sender)
     @friend_request.destroy
-
-    redirect_to user_path(current_user)
   end
 
   def reject
-    @friend_request = FriendRequest.find(params[:id])
+    @friend_request = FriendRequest.find(params[:friend_request_id])
+    @old_friend_id = @friend_request.sender.id
     @friend_request.destroy
-
-    redirect_to user_path(current_user)
   end
 
   private
