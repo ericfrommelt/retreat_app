27f4dcc7fb7e4768dfca8b2fe2d95e4052e19a6f
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-08-07 19:10:09 -0400

    Inserted Edit and Deleted Buttons on the getaway showpage

--- app/controllers/getaways_controller.rb
+++ app/controllers/getaways_controller.rb
@@ -16,6 +16,27 @@ class GetawaysController < ApplicationController
     @play_activities = @getaway.activities.where(category: 'play')
   end
 
+  def edit
+    @getaway = Getaway.find(params[:id])
+  end
+
+  def update
+    @getaway = Getaway.find(params[:id])
+    @getaway.update(getaways_params)
+    if @getaway.save
+      redirect_to getaway_path(@getaway)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @getaway = Getaway.find(params[:id])
+    @getaway.destroy
+    redirect_to user_path(@user)
+  end
+
   def getaways_params
     params.require(:getaway).permit(:city, :country, :comment, :departure_date, :user_id)
   end
